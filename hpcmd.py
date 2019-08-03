f9c2a55f59493bd82f241f4d68054bd79354c56f
Antoine Sauray <sauray.antoine@gmail.com>
2019-08-03 18:43:20 +0200

    add describe

--- hpcmd.py
+++ hpcmd.py
@@ -3,9 +3,10 @@ from cmd import Cmd
 from api import Api
 
 
-from features_descriptors import create_features
-from labels_descriptors import create_labels
+from features_descriptors import create_features, describe_feature
+from labels_descriptors import create_labels, describe_label, list_labels
 from project import create_project
+from algorithm import create_algorithm
  
 
 
@@ -34,7 +35,7 @@ class HyperplanPrompt(Cmd):
             if arg == 'features':
                 self.api.list_features()
             elif arg == 'labels':
-                self.api.list_labels()
+                list_labels(self.api)
             elif arg == 'algorithms':
                 self.api.list_algorithms()
             elif arg == 'projects':
@@ -44,8 +45,15 @@ class HyperplanPrompt(Cmd):
         else:
             self.help_list()
 
+    def help_describe(self):
+        print('describe requires the entity type (feature, label, algorithm, project) and the entity id')
     def help_create(self):
         print('create requires an argument: feature, label, algorithm, project')
+
+    def complete_describe(self, text, line, begidx, endidx):
+        return [i
+            for i in ('feature', 'label', 'algorithm', 'project')
+            if i.startswith(text)]
     def complete_create(self, text, line, begidx, endidx):
         return [i
             for i in ('feature', 'label', 'algorithm', 'project')
@@ -59,13 +67,31 @@ class HyperplanPrompt(Cmd):
             elif arg == 'label':
                 create_labels(self.api)
             elif arg == 'algorithm':
-                self.api.create_algorithm()
+                create_algorithm(self.api)
             elif arg == 'project':
                 create_project(self.api)
             else:
                 print('Unknown argument {}'.format(arg))
         else:
             self.help_create()
+    def do_describe(self, inp):
+        args = inp.split(' ')
+        if len(args) > 1 and args[0] != ''and args[1] != '':
+            arg = args[0]
+            entity_id = args[1]
+            if arg == 'feature':
+               describe_feature(self.api, entity_id)
+            elif arg == 'label':
+               describe_label(self.api, entity_id)
+            elif arg == 'algorithm':
+                pass
+            elif arg == 'project':
+                project = self.api.get_project(entity_id)
+                print(project)
+            else:
+                print('Unknown argument {}'.format(arg))
+        else:
+            self.help_describe()
     def help_exit(self):
         print('exit the application. Shorthand: x q Ctrl-D.')
     def default(self, inp):
